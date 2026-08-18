bfeb9ae10a8788c1e97658d0d316980bffb52e6b
deahzon-rgb <deahzon@gmail.com>
2026-08-17 21:13:10 -0600

    Create ProtectionStonesHook.java

---
+++ src/main/java/com/drstudio/psmenus/commands/​hooks/ProtectionStonesHook.java
@@ -0,0 +1,11 @@
+package com.drstudio.psmenus.hooks;
+
+import com.drstudio.psmenus.PSMenusPlugin;
+
+public class ProtectionStonesHook {
+    private final PSMenusPlugin plugin;
+
+    public ProtectionStonesHook(PSMenusPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
