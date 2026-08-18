72a64819154aa53cfd3b7fdd2fd91eb41ca4e8bd
deahzon-rgb <deahzon@gmail.com>
2026-08-17 21:07:04 -0600

    Create TeleportManager.java

---
+++ src/main/managers/TeleportManager.java
@@ -0,0 +1,17 @@
+package com.drstudio.psmenus.managers;
+
+import com.drstudio.psmenus.PSMenusPlugin;
+import org.bukkit.entity.Player;
+
+public class TeleportManager {
+
+    private final PSMenusPlugin plugin;
+
+    public TeleportManager(PSMenusPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void teleportToPS(Player player, String psName) {
+        // Lógica de teletransporte
+    }
+}
