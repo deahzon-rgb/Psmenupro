ee04ab4486b9025f72625e9946c5159640809504
deahzon-rgb <deahzon@gmail.com>
2026-08-17 21:13:58 -0600

    Create MenuManager.java

---
+++ src/main/java/com/drstudio/psmenus/commands/​hooks/​listeners/​managers/MenuManager.java
@@ -0,0 +1,15 @@
+package com.drstudio.psmenus.managers;
+
+import com.drstudio.psmenus.PSMenusPlugin;
+import org.bukkit.entity.Player;
+
+public class MenuManager {
+    private final PSMenusPlugin plugin;
+
+    public MenuManager(PSMenusPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void openMainMenu(Player player) {
+    }
+}
