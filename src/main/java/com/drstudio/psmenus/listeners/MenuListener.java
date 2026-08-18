f601806e7b5e4856729c01dd3be5c93022b4467c
deahzon-rgb <deahzon@gmail.com>
2026-08-17 21:25:57 -0600

    Create MenuListener.java

---
+++ src/main/java/com/drstudio/psmenus/listeners/MenuListener.java
@@ -0,0 +1,12 @@
+package com.drstudio.psmenus.listeners;
+
+import com.drstudio.psmenus.PSMenusPlugin;
+import org.bukkit.event.Listener;
+
+public class MenuListener implements Listener {
+    private final PSMenusPlugin plugin;
+
+    public MenuListener(PSMenusPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
